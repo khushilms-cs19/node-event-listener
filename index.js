7a214b4d27def9d81018f20ad36d3bca42d066b4
khushilms-cs19 <67272210+khushilms-cs19@users.noreply.github.com>
2023-05-23 22:37:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// express app boilerplate 
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const Joi = require('joi');
+const port = process.env.PORT || 3000;
+const axios = require('axios');
+
+app.use(express.json());
+app.use(cors());
+
+const bodySchema = Joi.object({
+  git_repo: Joi.string().required(),
+  git_branch: Joi.string().required(),
+  namespace: Joi.string().required(),
+  project: Joi.string().required(),
+});
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+app.post('/el-quarkus', async (req, res) => {
+  const { error } = bodySchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.message });
+  } else {
+    const response = await axios.post('http://a1c26ff7931e246a7b44b9670d3e81d7-458380765.us-east-1.elb.amazonaws.com:8080', req.body);
+    console.log(response);
+    res.json({ message: 'el-quarkus', body: req.body, response: response.data });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
