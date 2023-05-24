50a84fad69b6ef5f1f329fd791231c799f87714b
khushilms-cs19 <67272210+khushilms-cs19@users.noreply.github.com>
2023-05-24 09:04:34 +0530

    chore: add logs for the quarkus deployment

--- index.js
+++ index.js
@@ -22,12 +22,15 @@ app.get('/ping', (req, res) => {
 });
 
 app.post('/el-quarkus', async (req, res) => {
+  console.log(`Quarkus request received from remote address: ${req.socket.remoteAddress}`);
+  console.log('IP Address:' + req.ip);
+  console.log('Body:' + req.body);
   const { error } = bodySchema.validate(req.body);
   if (error) {
     return res.status(400).json({ message: error.message });
   } else {
     const response = await axios.post('http://a1c26ff7931e246a7b44b9670d3e81d7-458380765.us-east-1.elb.amazonaws.com:8080', req.body);
-    console.log(response);
+    console.log(response.data);
     res.json({ message: 'el-quarkus', body: req.body, response: response.data });
   }
 });
