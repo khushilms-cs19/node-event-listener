38f0e2fdbe47d45df5022d853afb19da53fc9538
khushilms-cs19 <67272210+khushilms-cs19@users.noreply.github.com>
2023-05-24 10:32:38 +0530

    chore: update the event listener link and add end point for the spring

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const bodySchema = Joi.object({
   git_branch: Joi.string().required(),
   namespace: Joi.string().required(),
   project: Joi.string().required(),
+  environment: Joi.string().required(),
 });
 
 app.get('/ping', (req, res) => {
@@ -24,17 +25,33 @@ app.get('/ping', (req, res) => {
 app.post('/el-quarkus', async (req, res) => {
   console.log(`Quarkus request received from remote address: ${req.socket.remoteAddress}`);
   console.log('IP Address:' + req.ip);
-  console.log('Body:' + req.body);
+  console.log('Body:', req.body);
   const { error } = bodySchema.validate(req.body);
   if (error) {
     return res.status(400).json({ message: error.message });
   } else {
-    const response = await axios.post('http://a1c26ff7931e246a7b44b9670d3e81d7-458380765.us-east-1.elb.amazonaws.com:8080', req.body);
+    const response = await axios.post('http://ae414b8b890e54cbeb7972e243a93d70-1041701709.us-east-1.elb.amazonaws.com:8080', req.body);
     console.log(response.data);
     console.log('before return');
     return res.json({ message: 'el-quarkus', body: req.body, response: response.data });
   }
 });
+
+app.post('/el-springboot', async (req, res) => {
+  console.log(`Springboot request received from remote address: ${req.socket.remoteAddress}`);
+  console.log('IP Address:' + req.ip);
+  console.log('Body:', req.body);
+  const { error } = bodySchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.message });
+  } else {
+    const response = await axios.post('http://a8c856d55cf834598b1413fda5143cf0-1816421437.us-east-1.elb.amazonaws.com:8080', req.body);
+    console.log(response.data);
+    console.log('before return');
+    return res.json({ message: 'el-springboot', body: req.body, response: response.data });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
